39a72a66b7105e38b992fd2abd312055ba6e624b
Loki <126240945+lokiaus@users.noreply.github.com>
2024-04-11 01:01:09 +1000

    Update HTML structure, Navbar, and Footer components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
 export default function Navbar() {
     return (
-        <>
-            <h1 className="flex justify-center text-white">WAR MANAGER</h1>
-        </>
+        <nav className="bg-gray-800 rounded-lg shadow m-4">
+            <div className="text-sm text-gray-400 w-full mx-auto p-4">
+                <span className="font-semibold flex justify-evenly">
+                    <h1 className=" font-extrabold flex justify-center">
+                        WAR MANAGER
+                    </h1>
+                    <a href="#" className="hover:underline">
+                        RECRUIT
+                    </a>
+                    <a href="#" className="hover:underline">
+                        ARM
+                    </a>
+                    <a href="#" className="hover:underline">
+                        FIGHT
+                    </a>
+                </span>
+            </div>
+        </nav>
     );
 }
